ca9925e44962d7088a8355b33a076bbfd2136347
saurabh <saurabhsahofficial@gmail.com>
2023-04-24 23:25:37 +0530

    completed today assignment

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
-# Technetium
-
-Nodejs Introduction Session
-
-## Assignment Link
-
-https://drive.google.com/drive/folders/18vvu0xJvveCrsE0qKejYKe_KImQwtmC9?usp=sharing
-
+### Assignment Question Link 
+https://docs.google.com/document/d/1Q1bkf_ruL3qGYOO-rINkVX62GR4Czy_HO6uy1P_9lhM/edit
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,45 +1,34 @@
 const express = require('express');
 const { route } = require('express/lib/application');
 const router = express.Router();
-const commonFile = require('./common');
-const logger = require("../logger/logger.js");
-const helper = require("../util/helper.js");
-const formatter = require("../validator/formatter");
-const lodash = require('lodash');
-router.get('/test-me', function (req, res) {
-    // Problem 1.
-    console.log("Problem 1.")
-    logger.welcome("Saurabh Kumar");
 
-    // Problem 2.
-    console.log("\nProblem 2.")
-    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-    helper.date();
-    helper.month();
-    helper.batchInfo("Technetium","W5","D4","Node js module system");
+let playerData = [
+    { name: "Manish",dob: "1/1/1995", gender: "male", city: "jalandhar", sports: ["swimming"] },
+    { name: "Ram",dob: "02/01/1999", gender: "male", city: "hariyana", sports: ["swimming","dancing"] },
+    { name: "Saurabh",dob: "08/10/2000", gender: "male", city: "bihar", sports: ["playing badminton"] },
+    { name: "Shivam",dob: "1/1/2010", gender: "male", city: "Dehli", sports: ["cricket"] },
+    { name: "Raja",dob: "1/1/2003", gender: "male", city: "gujrat", sports: ["swimming","lisenting music"] },
+    { name: "Pritesh",dob: "1/1/1985", gender: "male", city: "utter pradesh", sports: ["swimming","driving"] }
+]
 
-    // Problem 3.
-    console.log("\nProblem 3.")
-    formatter.trim(' functionup ')
-    formatter.changeToLowerCase('FUNCTIONUP')
-    formatter.changeToUpperCase('functionup')
-
-    // Problem 4.
-    console.log("\nProblem 4.")
-    const odd = [1,3,5,7,9,11,13,15,17,19];
-    const arr1=[1,2,3,4,5], arr2=[3,4,5,6,7],arr3=[5,6,7,8,9,10],arr4=[9,10,11,12,13],arr5=[13,14,15,16,17];
-    const keyValue = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]];
-    console.log("Lodash Chunk() call => ",lodash.chunk(month,4));
-    console.log("Lodash tail() call => ",lodash.tail(odd));
-    console.log("Lodash Union() call => ",lodash.union(arr1,arr2,arr3,arr4,arr5));
-    console.log("Lodash formPairs() call => ",lodash.fromPairs(keyValue));
-    res.send('This should be working!')
-});
-
-router.get('/test-you', function (req, res) {
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
-});
-
+function addData(data) {
+    let flag = true;
+    for(let i=0;i<playerData.length;i++) {
+        if(playerData[i].name == data.name) {
+            flag = false;
+            break;
+        }
+    }
+    if(flag == true) {
+        playerData.push(data)
+        return playerData;
+    } else {
+        return "Player already exits.";
+    }
+}
+router.post('/players', function(req,res) {
+    let playerDetails = { name: "Manish",dob: "1/1/1995", gender: "male", city: "jalandhar", sports: ["swimming"] }
+    res.send(addData(playerDetails))
+})
 
 module.exports = router;
